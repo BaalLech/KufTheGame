15ef26f4b6e7943d2fe33e9831a607cbc29a0b17
vesheff <martin.veshev@gmail.com>
2015-06-12 12:37:18 +0300

    Small change

--- KufTheGame/Models/Abstracts/Enemy.cs
+++ KufTheGame/Models/Abstracts/Enemy.cs
@@ -1,9 +1,45 @@
 ﻿namespace KufTheGame.Models.Abstracts
 {
-    public abstract class Enemy: Character
+    public abstract class Enemy : Character
     {
-        protected Enemy(int x, int y, double attackPoints, double defencePoints, double healthPoints) : base(x, y, attackPoints, defencePoints, healthPoints)
+        protected Enemy(int x, int y, double attackPoints, double defencePoints, double healthPoints)
+            : base(x, y, attackPoints, defencePoints, healthPoints)
         {
+        }
+
+        public override void Move()
+        {
+            
+        }
+
+        public override void Attack(Character target)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void RespondToAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int DrawOrder
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override bool Visible
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void ProduceSound()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
